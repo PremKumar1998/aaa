c8dd68e8df26563122db5e873fa4de4c262cdaab
PremKumar1998 <70958038+PremKumar1998@users.noreply.github.com>
2021-06-07 11:41:31 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>GTA lover</h1>
+      <p>
+        I love to play GTA. Whenever I get stressed I simly open my laptop and start play GTA vice city, which is one of the old game. 
+      </p>
+    </div>
+  );
+}
+
+export default Note;
